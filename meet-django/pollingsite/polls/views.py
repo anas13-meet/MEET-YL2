c056875ad6b987a863e3e6edd772db09fd734a00
anas13-meet <anas.kiswani2098@gmail.com>
2014-12-15 19:58:01 +0200

    Update views.py

--- meet-django/pollingsite/polls/views.py
+++ meet-django/pollingsite/polls/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from models import Poll, Choice
+def Hello(request): 
+	return render(request, ‘template_name.html’)
 # Create your views(functions) here.
 # Remember each function/view the first argument/input has to be request
