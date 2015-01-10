a19f1ae8feb3ed522bda2cff9cc10050a634baa6
anas13 <anas.kiswani2098@gmail.com>
2015-01-10 10:42:15 -0500

    Hallo

--- meet-django/pollingsite/polls/urls.py
+++ meet-django/pollingsite/polls/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-	url(r'^meet/$,views.hello, name="pizza")
+	url(r'^meet/$,views.index, name="meet")
--- meet-django/pollingsite/polls/views.py
+++ meet-django/pollingsite/polls/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from django.http import HttpResponse
 
 from models import Poll, Choice
-def Hello(request): 
-	return render(request, ‘template_name.html’)
 # Create your views(functions) here.
 # Remember each function/view the first argument/input has to be request
+def index(request):
+	return render(request, "about.html")
